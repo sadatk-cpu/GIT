6a8cdaedcc5ed159f57988ee7b542c0961a0d82c
vidhyakamalesh <vidhya.santhakumar253@gmail.com>
2019-07-04 17:22:43 +0530

    First Commit

---
+++ Maven-Project/src/main/java/com/selenium/test/Maven_Project/MavenClass.java
@@ -0,0 +1,21 @@
+package com.selenium.test.Maven_Project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavenClass {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();//initialize browser
+		driver.get("https://connect.maveric-systems.com/");
+		//Thread.sleep(500000);
+		WebElement ele=driver.findElement(By.name("yt0"));
+		driver.findElement(By.id("LoginForm_username")).sendKeys("shahistap");
+		driver.findElement(By.id("LoginForm_password")).sendKeys("shaMG@29");
+
+	}
+
+}
